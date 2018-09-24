4bf63c982bcd63997555fa5ec259bda1101c03fc
Bernardo Trevizan <trevizanbernardo@gmail.com>
2018-09-24 18:35:52 -0300

    Create admin dashboard for ease on adding data

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Admin)
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Rating)
+admin.site.register(Appointment)
+admin.site.register(DaySchedule)
+
