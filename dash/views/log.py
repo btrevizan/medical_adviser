5823a7c6f8069ad4a06a91b0ea72d78d8dd4fd7f
Bernardo Trevizan <trevizanbernardo@gmail.com>
2018-11-03 15:43:52 -0300

    Log a user out implemented

--- dash/views/log.py
+++ dash/views/log.py
@@ -1,5 +1,10 @@
+from django.contrib.auth import logout
 from django.views import generic
 
 
 class LogoutView(generic.TemplateView):
     template_name = 'registration/logout.html'
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return super().get(request, *args, **kwargs)
