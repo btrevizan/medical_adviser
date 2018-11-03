dc2b7c4e518558336c413f8a8dfa648336fbaab4
Bernardo Trevizan <trevizanbernardo@gmail.com>
2018-11-03 15:44:24 -0300

    Fixed bugs on doctor ratings list

--- dash/views/doctor/rating.py
+++ dash/views/doctor/rating.py
@@ -1,12 +1,9 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.utils.decorators import method_decorator
-from django.views.generic.edit import UpdateView
 from django.views.generic import TemplateView
-from django.http import HttpResponseRedirect
 from dash.decorators import doctor_required
-from django.urls import reverse_lazy
-from django.shortcuts import render
 from dash.models import Rating
+
 
 @method_decorator(doctor_required, name='dispatch')
 class RatingListView(PermissionRequiredMixin, TemplateView):
@@ -16,17 +13,13 @@ class RatingListView(PermissionRequiredMixin, TemplateView):
     permission_required = "dash.view_rating"
 
     def get_context_data(self, **kwargs):
-        # Joao Pedro.
-
-        #context = super().get_context_data(**kwargs)
-        #context[self.context_object_name] = self.model.objects\
-        #    .filter()\
-        #    .order_by('appointment__datetime')
+        context = super().get_context_data(**kwargs)
 
         doctor = self.request.user.doctor
-        context = {self.context_object_name: self.model.objects
-                                                .filter(appointment__doctor_id=doctor.id)
-                                                .order_by('-appointment__datetime')}
+        context[self.context_object_name] = self.model.objects.filter(appointment__doctor_id=doctor.id,
+                                                                      status=Rating.APPROVED)\
+            .order_by('-appointment__datetime')
+
         context['is_doctor'] = True
         return context
 
